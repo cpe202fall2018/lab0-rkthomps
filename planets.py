28a296bbe791cdc168753cf717a0297033321e73
Kyle Thompson <rkthomps@calpoly.edu>
2018-09-23 14:36:19 -0700

    Added Mars and Jupiter Calculations

--- planets.py
+++ planets.py
@@ -1,6 +1,7 @@
 def weight_on_planets():
    # write your code here
-   
+   weight = float(raw_input("What do you weigh on earth?"))
+   print "\nOn Mars you would weigh", round(0.38 * weight, 2), "pounds.\nOn Jupiter you would weigh", round(2.34 * weight, 2), "pounds."
    
    
 if __name__ == '__main__':
