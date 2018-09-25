2008a352f2f195713683f0cae4dfec90833f3db0
Kyle Thompson <rkthomps@calpoly.edu>
2018-09-24 19:06:30 -0700

    Tried to get correct formatting

--- planets.py
+++ planets.py
@@ -7,11 +7,14 @@
 # Section
 # Purpose: Create a program that takes in weight on earth and outputs weight on Mars and Jupiter
 
+
+#This function prints a persons weight on Mars and Jupiter given their weight on earth
 def weight_on_planets():
-   # write your code here
-   weight = float(raw_input("What do you weigh on earth?"))
-   print "\nOn Mars you would weigh", round(0.38 * weight, 2), "pounds.\nOn Jupiter you would weigh", round(2.34 * weight, 2), "pounds."
-   
+   # weight is the weight in pounds of the user on earth
+   weight = float(input("What do you weigh on earth? "))
+   #print("\nOn Mars you would weigh 51.68 pounds.\nOn Jupiter you would weigh 318.24 pounds.")
+   print ("\nOn Mars you would weigh",weight * .38,"pounds.\nOn Jupiter you would weigh",weight * 2.34,"pounds.")
+
    
 if __name__ == '__main__':
    weight_on_planets()
